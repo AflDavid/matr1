8b6b6981a5f55ee427a5419c096a68153c5304c2
AflDavid <93366100+AflDavid@users.noreply.github.com>
2021-12-02 23:51:03 +0200

    Add files via upload

---
+++ matric1/matric1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace matric1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linii-");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[,] matr = new int[N, N];
+
+            bool matrIdentitate = true;
+
+            for (int row = 0; row < matr.GetLength(0); row++)
+
+                for (int col = 0; col < matr.GetLength(1); col++)
+                {
+                    Console.Write($"[{row},{col}]:");
+                    matr[row, col] = Convert.ToInt32(Console.ReadLine());
+
+                    if (row != col)
+                    {
+                        if (matr[row, col] != 0)
+                        {
+                            matrIdentitate = false;
+                        }
+                    }
+                    else if (matr[row, col] != 1)
+                    {
+                        matrIdentitate = false;
+                    }
+                }
+            if (matrIdentitate == true)
+                Console.WriteLine("Adevarat");
+            else
+                Console.WriteLine("Fals");
+
+
+        }
+    }
+}
